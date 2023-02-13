e08c113379921a7022064aac85de5639ffd68435
nizam754 <m.nizam754@gmail.com>
2023-02-13 21:59:41 +0600

    Manage files

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask, render_template
+app = Flask(__name__)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route("/")
+def home():
+    return render_template('index.html')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+
+app.run(debug=True)
+
