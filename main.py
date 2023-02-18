448a38c79d3137a2f1a8fefae1705bd7c17ac7ae
Mostafa Nizam <m.nizam754@gmail.com>
2023-02-18 17:04:20 +0600

    Making Parameters Configurable

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import json
 from datetime import datetime
 
+
+with open('config.json', 'r') as c:
+    params = json.load(c)["params"]
+
+local_server = True
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:@localhost/infotechtu'
+if local_server:
+    app.config["SQLALCHEMY_DATABASE_URI"] = params['local_uri']
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = params['prod_rui']
+
 db = SQLAlchemy(app)
 
 
 class Contacts(db.Model):
-    """
-    sno, name,email, phone_num, msg, date
-    """
     sno = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(20))
@@ -21,18 +28,17 @@ class Contacts(db.Model):
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', params=params)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', params=params)
 
 
 @app.route("/contact", methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
-        '''Add entry to the database'''
         name = request.form.get('name')
         email = request.form.get('email')
         phone = request.form.get('phone')
@@ -40,7 +46,7 @@ def contact():
         entry = Contacts(name=name, phone_num=phone, msg=message, date= datetime.now(), email=email)
         db.session.add(entry)
         db.session.commit()
-    return render_template('contact.html')
+    return render_template('contact.html', params=params)
 
 
 app.run(debug=True)
