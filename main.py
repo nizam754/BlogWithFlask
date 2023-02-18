9e65b90ce81a49655934cc97e89ed716452ca10f
Mostafa Nizam <m.nizam754@gmail.com>
2023-02-19 01:01:28 +0600

    Fetching Posts From Database

--- main.py
+++ main.py
@@ -34,9 +34,24 @@ class Contacts(db.Model):
     date = db.Column(db.String(12), nullable=True)
 
 
+class Posts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=False, nullable=False)
+    slug = db.Column(db.String(25))
+    content = db.Column(db.String(120), unique=True, nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    img_file = db.Column(db.String(12), nullable=True)
+
+
 @app.route("/")
 def home():
     return render_template('index.html', params=params)
+
+
+@app.route("/post/<string:post_slug>", methods=['GET'])
+def post_route(post_slug):
+    post = Posts.query.filter_by(slug=post_slug).first()
+    return render_template('post.html', params=params, post=post)
 
 
 @app.route("/about")
@@ -57,7 +72,7 @@ def contact():
         mail.send_message(
             'New message from ' + name,
             sender=email,
-            recipients = [params['gmail-user']],
+            recipients=[params['gmail-user']],
             body=message + "\n" + phone
         )
     return render_template('contact.html', params=params)
